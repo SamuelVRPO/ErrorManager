c5f0dbce823930bb5e930ea0410712df3991f887
Samuel Vieira <samuelvieirarpo@gmail.com>
2020-09-11 11:28:03 -0500

    Initial Commit

---
+++ assignment13/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assignment13
+//
+//  Created by Samuel Vieira on 11/09/20.
+//  Copyright © 2020 Samuel Vieira Restrepo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
